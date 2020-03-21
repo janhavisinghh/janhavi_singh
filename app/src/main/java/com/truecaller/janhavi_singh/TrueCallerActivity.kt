91e331f291e3785d61edb9063a35fe490fb11a39
Janhavi Singh <janhavi.shanu@gmail.com>
2020-03-21 16:13:08 +0530

    Enable Data Binding Library

--- app/src/main/java/com/truecaller/janhavi_singh/TrueCallerActivity.kt
+++ app/src/main/java/com/truecaller/janhavi_singh/TrueCallerActivity.kt
@@ -1,12 +1,14 @@
 package com.truecaller.janhavi_singh
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.truecaller.janhavi_singh.databinding.ActivityTruecallerBinding
 
 class TrueCallerActivity : AppCompatActivity() {
-
+    var binding: ActivityTruecallerBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_truecaller)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_truecaller)
     }
 }
